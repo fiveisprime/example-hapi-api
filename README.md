f8b3766ea135d691c2efcc5df261d2ae9973ef39
Matt Hernandez <matt@modulus.io>
2013-07-16 20:51:48 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+Example API using Hapi
+===
+
+Based on the [Modulus blog post](http://blog.modulus.io/nodejs-and-express-create-rest-api) about building a REST API using Express.. except using Hapi.
+
+_Note: requires node version 0.10.x_
---
+++ server.js
@@ -0,0 +1,87 @@
+//
+//      Example API using Hapi.
+//      http://hapijs.com
+//
+
+var Hapi = require('hapi');
+
+var quotes = [
+  {
+    author: 'Audrey Hepburn'
+  , text: 'Nothing is impossible, the word itself says \'I\'m possible\'!'
+  }
+, {
+    author: 'Walt Disney'
+  , text: 'You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you'
+  }
+, {
+    author: 'Unknown'
+  , text: 'Even the greatest was once a beginner. Don\'t be afraid to take that first step.'
+  }
+, {
+    author: 'Neale Donald Walsch'
+  , text: 'You are afraid to die, and you\'re afraid to live. What a way to exist.'
+  }
+];
+
+var server = Hapi.createServer('localhost', process.env.PORT || 3000);
+
+//
+// Simulate an external module which is the correct way to expose this
+//    kind of functionality.
+//
+var quoteController = {};
+
+quoteController.get = {
+  handler: function(req) {
+    if (req.params.id) {
+      if (quotes.length <= req.params.id) return req.reply('No quote found.').code(404);
+      req.reply(quotes[req.params.id]);
+    }
+    req.reply(quotes);
+  }
+};
+
+quoteController.post = {
+  handler: function(req) {
+    quotes.push({ author: req.payload.author, text: req.payload.text });
+    req.reply();
+  }
+, validate: {
+    payload: {
+      author: Hapi.types.String().required()
+    , text: Hapi.types.String().required()
+    }
+  }
+};
+
+quoteController.delete = {
+  handler: function(req) {
+    if (quotes.length <= req.params.id) return req.reply('No quote found.').code(404);
+    quotes.splice(req.params.id, 1);
+    req.reply();
+  }
+};
+
+//
+// Configure the routes.
+//
+server.route({
+  path: '/quote/{id?}'
+, method: 'GET'
+, config: quoteController.get
+});
+
+server.route({
+  path: '/quote'
+, method: 'POST'
+, config: quoteController.post
+});
+
+server.route({
+  path: '/quote/{id}'
+, method: 'DELETE'
+, config: quoteController.delete
+});
+
+server.start();
